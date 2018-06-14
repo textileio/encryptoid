55da863cc97a6e153ed2ad0d854271049091f051
Carson Farmer <carson.farmer@gmail.com>
2018-06-13 23:20:58 -0700

    initial public code commit

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Encryptoid
+# Encryptoid IPFS ĐApp
 
 > Browser ĐApp for encrypting and sending ephemeral secret messages over IPFS
+
+Its almost impossible to share digital information without leaving a trace. Almost. This ĐApp allows you to create emphemeral secret messages, encode them with industry standard encryption algoritms, and share them with friends... until the messages disappear _forever_.
+
+## Install
+
+1.  This app works best with `window.ipfs`. Install the IPFS Companion web extension:
+
+    <a href="https://addons.mozilla.org/en-US/firefox/addon/ipfs-companion/" title="Get the add-on"><img width="86" src="https://blog.mozilla.org/addons/files/2015/11/AMO-button_1.png" /></a> <a href="https://chrome.google.com/webstore/detail/ipfs-companion/nibjojkomfdiaoajekhjakgkdhaomnch" title="Get the extension"><img width="103" src="https://developer.chrome.com/webstore/images/ChromeWebStore_BadgeWBorder_v2_206x58.png" /></a>
+
+2.  Install dependencies `yarn install`
+3.  Build the app `yarn build`
+4.  Start the app `yarn start`
+
+### Develop
+
+Instead of steps 3 & 4: `yarn watch`
+
+### Thanks
+
+Built using the steps outlined in [this blog post]().
---
+++ src/main.js
@@ -0,0 +1,75 @@
+import 'babel-polyfill'
+import getIpfs from 'window.ipfs-fallback'
+import url from 'url'
+import crypto from 'libp2p-crypto'
+
+let ipfs
+
+const setup = async () => {
+  const parts = url.parse(window.location.href, true)
+  const query = parts.query
+  const button = document.getElementById('cryptofy')
+  const message = document.getElementById('message')
+  const password = document.getElementById('password')
+  const output = document.getElementById('output')
+  const base = window.location.origin + window.location.pathname
+  const isDecrypting = Object.keys(query).length > 0
+
+  // Modify share links with _this_ url
+  for (let element of document.getElementsByClassName('linkable')) {
+    element.href = element.href + encodeURI(base)
+  }
+  try {
+    ipfs = await getIpfs()
+    ipfs.swarm.connect(
+      '/dns4/BrowserConnector-2058342286.us-east-1.elb.amazonaws.com/tcp/443/wss/ipfs/QmPYxbNHdCN3ervZobHSDrN9v9XkzUL8QxDJSpATJLCwki'
+    )
+    // Show button when ready
+    if (isDecrypting) {
+      button.innerHTML = 'Decrypt'
+      const file = (await ipfs.files.get(`/ipfs/${query.cid}`))[0]
+      const content = file.content.toString('base64')
+      document.getElementById('message').value = content
+    }
+    button.style.visibility = 'visible'
+    button.addEventListener('click', (e) => {
+      output.innerHTML = ''
+      if (!password.validity.valid) {
+        return
+      }
+      e.preventDefault()
+      // Compute a derived key to use in AES encryption algorithm
+      // We aren't ever storing passwords, so no need to worry about salt
+      const key = crypto.pbkdf2(password.value, 'encryptoid', 5000, 24, 'sha2-256')
+      // We're only using the key once, so a fixed IV should be ok
+      const iv = Buffer.from([...Array(16).keys()])
+      // Create AES encryption object
+      crypto.aes.create(Buffer.from(key), iv, (err, cipher) => {
+        if (!err) {
+          if (isDecrypting) {
+            cipher.decrypt(Buffer.from(message.value, 'base64'), async (err, plaintext) => {
+              if (!err) {
+                const info = `Your super secret message is:
+                `
+                const msg = `${plaintext.toString('utf-8')}`
+                output.innerText = info + '"' + msg + '"'
+              }
+            })
+          } else {
+            cipher.encrypt(Buffer.from(message.value), async (err, encrypted) => {
+              if (!err) {
+                const hashed = (await ipfs.files.add(encrypted))[0]
+                const info = `Send the following link to your recipient, and get social down below:<br/><br/>`
+                const msg = `<a href="${base}?cid=${hashed.hash}">${hashed.hash}</a>`
+                output.innerHTML = info + msg
+              }
+            })
+          }
+        }
+      })
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+setup()
